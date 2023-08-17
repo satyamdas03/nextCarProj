e78e93557b6ce5b5a26d5e6842578bd0d5c0e3ec
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-08-17 12:36:01 +0530

    g

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,9 @@ const Hero = () => {
   return (
     <div className='hero'>
         <div className='flex-1 pt-36 padding-x'>
-
+            <h1 className='hero__title'>
+                Find, Book, rent a car -- quickly and easily !!
+            </h1>
         </div>
     </div>
   )
