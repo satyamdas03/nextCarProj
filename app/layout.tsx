906d4ad233917d61f48314a42d9cd85bcb4d17d2
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-08-26 19:47:31 +0530

    p

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="relative">
         {children}
       </body>
     </html>
