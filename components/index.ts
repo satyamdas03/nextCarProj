caad75d8056f6def06c44d54c0d67b1d54eee2b2
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-08-27 21:32:29 +0530

    o

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,7 @@
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
-import CustomButton from "./CustomButton";
-import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export {
     Hero,
