f960d1e43857edc590d148bc1c3f721734b2253c
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-08-28 15:53:56 +0530

    ooooooo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,8 +7,8 @@ import CustomButton  from "./CustomButton";
 const Navbar = () => {
   return (
     <header className="w-full absolute z-10">
-      <nav>
-        
+      <nav className="max-w-[1440px] mx-auto flex justify-between">
+
       </nav>
     </header>
   )
