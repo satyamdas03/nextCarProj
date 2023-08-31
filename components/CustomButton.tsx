4875138b5bc55cf14e839c40ccbe9b60499725ed
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-08-31 11:16:08 +0530

    l

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image"
 import { CustomButtonProps } from "@/types";
 
-const CustomButton = ({ title, containerStyles, handleClick}: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType}: CustomButtonProps) => {
   return (
     <button
         disabled={false}
