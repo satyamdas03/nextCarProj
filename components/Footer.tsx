3e82dfca7e48dd429ba10d1f86502f6907e280df
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-08-31 11:32:37 +0530

    l

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer>FOOTer</footer>
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      FOOTer
+    </footer>
   )
 }
 
