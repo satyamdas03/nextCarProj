a9abcd8479a34d90225703d386c269b7e28227d2
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-09-08 12:02:52 +0530

    l

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { footerLinks } from "@/constants";
+
 const Footer = () => {
   return (
     <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
