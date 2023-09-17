7646ebac7db47aa18b3c80cc50f0d6844db143ce
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-09-17 18:01:33 +0530

    q

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,6 +19,12 @@ const Footer = () => {
           {footerLinks.map((link)=>(
             <div key={link.title} className="footer__link">
               <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item)=>(
+                <Link
+                  key={item.title}
+                  href={item.url}
+                />
+              ))}
             </div>
           ))}
         </div>
